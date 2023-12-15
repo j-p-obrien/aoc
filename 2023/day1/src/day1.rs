80685d8bae3566834670f3fd7b2d894651a492a0
JP O'Brien <jpobrien925@gmail.com>
2023-12-15 12:49:47 -0800

    First commit for Advent of Code 2023 day 1

---
+++ 2023/day1/src/day1.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct Config {
+    first: char,
+    last: Option<char>,
+}
+
+impl Config {
+    pub fn from_str(data: &str) -> Config {
+        todo!()
+    }
+}
